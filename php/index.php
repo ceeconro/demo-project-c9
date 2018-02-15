dd80e39f4cc8961b02ba3cdbb4cea9fcc12adca3
ceeconro <ceeconro@gmail.com>
2018-02-15 15:09:25 +0000

    init git project

--- php/index.php
+++ php/index.php
@@ -3,6 +3,14 @@
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
+echo 'Hello world from Cloud9 -prueba martes! <br>';
+
+$db = new mysqli('127.0.0.1', 'ceeconro', '', 'c9', '3306');
+if ($db->connect_errno > 0) {
+die('Error al conectarse a la base de datos'.$db->connect_error);
+} else {
+die('Conectado!');
+}
+
 
 ?>
